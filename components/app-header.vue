c9624ce23343927864f1ebe5b46dcfb56e783f65
Spimi <spimi.mail@gmail.com>
2024-12-12 13:49:53 +0100

    start

---
+++ components/app-header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="flex justify-between items-center mt-10">
+    <NuxtLink to="/" class="text-xl font-bold">
+      Finace Tracker
+    </NuxtLink>
+    <div>
+      <UAvatar src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNGZpNXM0MDhoeDdkajc1dTdmbmY5cHAxZ2l5bDVqODRvbG85d3Q2MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2kkdSiVkuKHzsCuou2/giphy.gif" alt="Avatar" />
+
+    </div>
+  </header>
+</template>
