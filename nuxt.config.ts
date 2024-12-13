cbbda1db0829564f92f45d3638940a4ff5ce21f7
Spimi <spimi.mail@gmail.com>
2024-12-13 17:19:27 +0100

    001

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
+  modules: ['@nuxt/ui', '@nuxtjs/supabase'],
+  supabase: {
+    redirect: false
+  }
 })
