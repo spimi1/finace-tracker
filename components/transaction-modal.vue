6c93e40913c5f947fa407af178e5633b982f6b16
Spimi <spimi.mail@gmail.com>
2024-12-20 22:42:50 +0100

    007

--- components/transaction-modal.vue
+++ components/transaction-modal.vue
@@ -1,133 +1,186 @@
 <template>
   <UModal v-model="isOpen">
     <UCard>
-      <template #header>
-        Add Transaction
-      </template>
-      <UForm :state="state" :schema="schema" ref="form" >
-
+      <template #header> Add Transaction </template>
+      <UForm :state="state" :schema="schema" ref="form">
         <!-- Transakcje -->
-        <UFormGroup :required="true" label="Transaction Type" name="type" class="mb-4" >
-          <USelect placeholder="Select the transaction type" :options="types" icon="i-line-md:check-list-3-twotone" v-model="state.type"/>
+        <UFormGroup
+          :required="true"
+          label="Transaction Type"
+          name="type"
+          class="mb-4"
+        >
+          <USelect
+            placeholder="Select the transaction type"
+            :options="types"
+            icon="i-line-md:check-list-3-twotone"
+            v-model="state.type"
+          />
         </UFormGroup>
 
         <!-- Kwota -->
-        <UFormGroup label="Amount" :required="true" name="amount" class="mb-4" >
-          <UInput tpye="number" placeholder="Amount" icon="i-line-md:briefcase" v-model.number="state.amount"/>
+        <UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
+          <UInput
+            tpye="number"
+            placeholder="Amount"
+            icon="i-line-md:briefcase"
+            v-model.number="state.amount"
+          />
         </UFormGroup>
 
         <!-- Data -->
-        <UFormGroup label="Transaction date" :required="true" name="created_at" class="mb-4" >
-          <UInput type="date" icon="i-line-md:calendar" v-model="state.created_at" />
+        <UFormGroup
+          label="Transaction date"
+          :required="true"
+          name="created_at"
+          class="mb-4"
+        >
+          <UInput
+            type="date"
+            icon="i-line-md:calendar"
+            v-model="state.created_at"
+          />
         </UFormGroup>
 
         <!-- Opis -->
-        <UFormGroup label="Description" hint="Optional" name="description" class="mb-4"  >
-          <UInput placeholder="Description" icon="i-line-md:align-left" v-model="state.description" />
+        <UFormGroup
+          label="Description"
+          hint="Optional"
+          name="description"
+          class="mb-4"
+        >
+          <UInput
+            placeholder="Description"
+            icon="i-line-md:align-left"
+            v-model="state.description"
+          />
         </UFormGroup>
 
         <!-- Kategoria -->
-        <UFormGroup :required="true" label="Category" name="category" class="mb-4" v-if="state.type == 'Expense'" >
-          <USelect placeholder="Category" :options="categories" icon="i-line-md:folder" v-model="state.category"/>
+        <UFormGroup
+          :required="true"
+          label="Category"
+          name="category"
+          class="mb-4"
+          v-if="state.type == 'Expense'"
+        >
+          <USelect
+            placeholder="Category"
+            :options="categories"
+            icon="i-line-md:folder"
+            v-model="state.category"
+          />
         </UFormGroup>
 
-        <UButton type="button" color="black" variant="solid" label="Save" :loading="isLoading" @click="save" />
+        <UButton
+          type="button"
+          color="black"
+          variant="solid"
+          label="Save"
+          :loading="isLoading"
+          @click="save"
+        />
       </UForm>
     </UCard>
   </UModal>
 </template>
 
-
 <script setup>
-import { categories, types } from '~/constants'
-import { z } from 'zod'
+import { categories, types } from "~/constants";
+import { z } from "zod";
 
 const props = defineProps({
-  modelValue: Boolean
-})
-const emit = defineEmits(['update:modelValue', 'saved'])
+  modelValue: Boolean,
+});
+const emit = defineEmits(["update:modelValue", "saved"]);
 
 const defaultSchema = z.object({
   created_at: z.string(),
   description: z.string().optional(),
-  amount: z.number().positive('Amount needs to be more than 0')
-})
+  amount: z.number().positive("Amount needs to be more than 0"),
+});
 
 const incomeSchema = z.object({
-  type: z.literal('Income')
-})
+  type: z.literal("Income"),
+});
 const expenseSchema = z.object({
-  type: z.literal('Expense'),
-  category: z.enum(categories)
-})
+  type: z.literal("Expense"),
+  category: z.enum(categories),
+});
 const investmentSchema = z.object({
-  type: z.literal('Investment')
-})
+  type: z.literal("Investment"),
+});
 const savingSchema = z.object({
-  type: z.literal('Saving')
-})
+  type: z.literal("Saving"),
+});
 
 const schema = z.intersection(
-  z.discriminatedUnion('type', [incomeSchema, expenseSchema, investmentSchema, savingSchema]),
+  z.discriminatedUnion("type", [
+    incomeSchema,
+    expenseSchema,
+    investmentSchema,
+    savingSchema,
+  ]),
   defaultSchema
-)
+);
 
-const form = ref()
-const isLoading = ref(false)
-const supabase = useSupabaseClient()
-const toast = useToast()
+const form = ref();
+const isLoading = ref(false);
+const supabase = useSupabaseClient();
+const toast = useToast();
 
 const save = async () => {
-  if (form.value.errors.length) return
+  if (form.value.errors.length) return;
 
-  isLoading.value = true
+  isLoading.value = true;
   try {
-    const { error } = await supabase.from('transactions')
-      .upsert({ ...state.value })
+    const { error } = await supabase
+      .from("transactions")
+      .upsert({ ...state.value });
 
     if (!error) {
       toast.add({
-        'title': 'Transaction saved',
-        'icon': 'i-line-md:circle-to-confirm-circle-twotone-transition'
-      })
-      isOpen.value = false
-      emit('saved')
-      return
+        title: "Transaction saved",
+        icon: "i-line-md:circle-to-confirm-circle-twotone-transition",
+      });
+      isOpen.value = false;
+      emit("saved");
+      return;
     }
 
-    throw error
+    throw error;
   } catch (e) {
     toast.add({
-      title: 'Transaction not saved',
+      title: "Transaction not saved",
       description: e.message,
-      icon: 'i-line-md:close-circle-twotone',
-      color: 'red'
-    })
+      icon: "i-line-md:close-circle-twotone",
+      color: "red",
+    });
   } finally {
-    isLoading.value = false
+    isLoading.value = false;
   }
-}
+};
 
 const initialState = {
   type: undefined,
   amount: 0,
   created_at: undefined,
   description: undefined,
-  category: undefined
-}
+  category: undefined,
+};
 const state = ref({
-  ...initialState
-})
+  ...initialState,
+});
 const resetForm = () => {
-  Object.assign(state.value, initialState)
-  form.value.clear()
-}
+  Object.assign(state.value, initialState);
+  form.value.clear();
+};
 
 const isOpen = computed({
   get: () => props.modelValue,
   set: (value) => {
-    if (!value) resetForm()
-    emit('update:modelValue', value)
-  }
-})
+    if (!value) resetForm();
+    emit("update:modelValue", value);
+  },
+});
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,73 @@
 <template>
   <section class="flex items-center justify-between mb-10">
-    <h1 class="text-4xl font-extrabold">
-      Summary
-    </h1>
+    <h1 class="text-4xl font-extrabold">Summary</h1>
     <div>
       <USelectMenu :options="transactionViewOptions" v-model="selectedView" />
     </div>
   </section>
 
-  <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
-    <Trend color="green" title="Income" :amount="incomeTotal" :last-amount="4100" :loading="isLoading" />
-    <Trend color="red" title="Expense" :amount="expenseTotal" :last-amount="3800" :loading="isLoading" />
-    <Trend color="green" title="Investments" :amount="4000" :last-amount="3000" :loading="isLoading" />
-    <Trend color="red" title="Saving" :amount="4000" :last-amount="4100" :loading="isLoading" />
+  <section
+    class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10"
+  >
+    <Trend
+      color="green"
+      title="Income"
+      :amount="incomeTotal"
+      :last-amount="prevIncomeTotal"
+      :loading="pending"
+    />
+    <Trend
+      color="red"
+      title="Expense"
+      :amount="expenseTotal"
+      :last-amount="prevExpenseTotal"
+      :loading="pending"
+    />
+    <Trend
+      color="green"
+      title="Investments"
+      :amount="4000"
+      :last-amount="3000"
+      :loading="pending"
+    />
+    <Trend
+      color="red"
+      title="Saving"
+      :amount="4000"
+      :last-amount="4100"
+      :loading="pending"
+    />
   </section>
 
   <section class="flex justify-between mb-10">
     <div>
       <h2 class="text-2xl font-extrabold">Transactions</h2>
       <div class="text-gray-500 dark:text-gray-400">
-        You have {{ incomeCount }} incomes and {{ expenseCount }} expenses this period
+        You have {{ incomeCount }} incomes and {{ expenseCount }} expenses this
+        period
       </div>
     </div>
     <div>
-      <TransactionModal v-model="isOpen" @saved="refreshTransactions()" />
-      <UButton icon="i-heroicons-plus-circle" color="white" variant="solid" label="Add" @click="isOpen = true" />
+      <TransactionModal v-model="isOpen" @saved="refresh()" />
+      <UButton
+        icon="i-heroicons-plus-circle"
+        color="white"
+        variant="solid"
+        label="Add"
+        @click="isOpen = true"
+      />
     </div>
   </section>
 
-  <section v-if="!isLoading">
-    <div v-for="(transactionsOnDay, date) in transactionsGroupedByDate" :key="date" class="mb-10">
+  <section v-if="!pending">
+    <div v-for="(transactionsOnDay, date) in byDate" :key="date" class="mb-10">
       <DailyTransactionSummary :date="date" :transactions="transactionsOnDay" />
-      <Transaction v-for="transaction in transactionsOnDay" :key="transaction.id" :transaction="transaction"
-        @deleted="refreshTransactions()" />
+      <Transaction
+        v-for="transaction in transactionsOnDay"
+        :key="transaction.id"
+        :transaction="transaction"
+        @deleted="refresh()"
+      />
     </div>
   </section>
   <section v-else>
@@ -41,80 +76,28 @@
 </template>
 
 <script setup>
-import { transactionViewOptions } from '~/constants'
-const supabase = useSupabaseClient()
+import { transactionViewOptions } from "~/constants";
 
-const selectedView = ref(transactionViewOptions[1])
-const transactions = ref([])
-const isLoading = ref(false)
-const isOpen = ref(false)
+const selectedView = ref(transactionViewOptions[1]);
+const isOpen = ref(false);
+const { current, previous } = useSelectedTimePeriod(selectedView);
 
-const income = computed(
-  () => transactions.value.filter(t => t.type === 'Income')
-)
-const expense = computed(
-  () => transactions.value.filter(t => t.type === 'Expense')
-)
+const {
+  pending,
+  refresh,
+  transactions: {
+    incomeCount,
+    expenseCount,
+    incomeTotal,
+    expenseTotal,
+    grouped: { byDate },
+  },
+} = useFetchTransactions(current);
 
-const incomeCount = computed(() => income.value.length)
-const expenseCount = computed(() => expense.value.length)
-
-const incomeTotal = computed(
-  () => income.value.reduce((sum, transaction) => sum + transaction.amount, 0)
-)
-const expenseTotal = computed(
-  () => expense.value.reduce((sum, transaction) => sum + transaction.amount, 0)
-)
-
-const fetchTransactions = async () => {
-  isLoading.value = true
-  try {
-    const { data } = await useAsyncData('transactions', async () => {
-      const { data, error } = await supabase
-        .from('transactions')
-        .select()
-        .order('created_at', { ascending: false })
-
-      if (error) return []
-
-      return data
-    })
-
-    return data.value
-  } finally {
-    isLoading.value = false
-  }
-}
-
-const refreshTransactions = async () => transactions.value = await fetchTransactions()
-
-await refreshTransactions()
-
-const transactionsGroupedByDate = computed(() => {
-  let grouped = {}
-
-  for (const transaction of transactions.value) {
-    const date = new Date(transaction.created_at).toISOString().split('T')[0]
-
-    if (!grouped[date]) {
-      grouped[date] = []
-    }
-
-    grouped[date].push(transaction)
-  }
-
-  // const sortedKeys = Object.keys(grouped).sort().reverse()
-  // const sortedGrouped = {}
-
-  // for (const key of sortedKeys) {
-  //   sortedGrouped[key] = grouped[key]
-  // }
-
-  // return sortedGrouped
-
-  return grouped
-})
-
-console.log(transactionsGroupedByDate.value)
-
+const {
+  transactions: {
+    incomeTotal: prevIncomeTotal,
+    expenseTotal: prevExpenseTotal,
+  },
+} = useFetchTransactions(previous);
 </script>
